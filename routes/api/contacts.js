cb72ea97b0e4090756fa41023e70de1f184b6f31
tarsweb <tarsweb@ukr.net>
2022-11-25 17:59:18 +0200

    refactoring

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,40 +4,38 @@ import { listContacts, getContactById, addContact, removeContact, updateContact 
 
 const router = Router()
 
-router.get('/', async (req, res, next) => {
+router
+.get('/', async (req, res, next) => {
   // res.json({ message: 'template message' })
   res.json(await listContacts())
 })
-
-router.get('/:contactId', async (req, res, next) => {
-  // res.json({ message: 'template message' })
-  const { contactId } = req.params
-  const contact = await getContactById(contactId);
-  if (contact) return res.json(contact)
-  next()
-})
-
-router.post('/', async (req, res, next) => {
+.post('/', async (req, res, next) => {
   // res.json({ message: 'template message' })
   const contact = await addContact(req.body);
   if (contact) return res.status(201).json(contact)
   next()
 })
 
-router.delete('/:contactId', async (req, res, next) => {
+router
+.get('/:contactId', async (req, res, next) => {
   // res.json({ message: 'template message' })
   const { contactId } = req.params
-  if (await removeContact(contactId)) return res.status(200).json({ message : "contact deleted"})
+  const contact = await getContactById(contactId);
+  if (contact) return res.json(contact)
   next()
 })
-
-router.put('/:contactId', async (req, res, next) => {
+.delete('/:contactId', async (req, res, next) => {
   // res.json({ message: 'template message' })
   const { contactId } = req.params
-  console.log(req.body);
+  if (await removeContact(contactId)) return res.json({ message : "contact deleted"})
+  next()
+})
+.put('/:contactId', async (req, res, next) => {
+  // res.json({ message: 'template message' })
+  const { contactId } = req.params
   if (! Object.keys(req.body).length) return res.status(400).json({message : "missing fields"})
   const contact = await updateContact(contactId, req.body)
-  if (contact) return res.status(200).json(contact)
+  if (contact) return res.json(contact)
   next()
 })
 
