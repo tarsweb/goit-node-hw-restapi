f615d6d1d87369f5f691cefe2635e72b675a50d9
tarsweb <58361241+tarsweb@users.noreply.github.com>
2022-12-12 20:58:25 +0200

    page limit favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,11 +1,14 @@
-const { Contact } = require('../../models/contact')
+const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const {_id: owner} = req.user;
-  const {page = 1, limit = 10} = req.query;
-  const skip = 0; // const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt, -owner", {skip, limit: Number(limit)});
-  res.json({status : "success", data: result})
-}
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10, favorite = "" } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner, favorite }, "-createdAt -updatedAt -owner", {
+    skip,
+    limit: Number(limit),
+  });
+  res.json({ status: "success", data: result });
+};
 
-module.exports = getAll
+module.exports = getAll;
